f9832ead5d244a42ff9b41150903058b08fcbd9c
avi <hi@avi.im>
2017-03-19 22:49:17 +0530

    Refactor code

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
 	"time"
 
@@ -11,7 +10,10 @@ import (
 	"golang.org/x/oauth2"
 )
 
-const redirectURI = "http://localhost:8080/callback"
+const (
+	redirectURI = "http://localhost:8080/callback"
+	port        = 8080
+)
 
 var (
 	auth = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadPrivate, spotify.ScopePlaylistReadPrivate,
@@ -21,6 +23,11 @@ var (
 )
 
 func main() {
+	client := getSpotifyClient()
+	fmt.Println(client)
+}
+
+func getSpotifyClient() *spotify.Client {
 	config := readConfig()
 	if config.AccessToken == "" || config.RefreshToken == "" {
 		fmt.Println("No Config")
@@ -30,39 +37,33 @@ func main() {
 	} else {
 		fmt.Println("From Config")
 	}
-	client := auth.NewClient(&oauth2.Token{
+	token := &oauth2.Token{
+		Expiry:       time.Unix(config.TokenExpiry, 0),
+		TokenType:    config.TokenType,
 		AccessToken:  config.AccessToken,
 		RefreshToken: config.RefreshToken,
-		Expiry:       time.Unix(config.TokenExpiry, 0),
-	})
+	}
+	client := auth.NewClient(token)
 	user, err := client.CurrentUser()
 	if err != nil {
-		log.Fatal("Shit :", err)
+		log.Fatal("Failed :", err)
 	}
 	fmt.Println("Hi: ", user.ID)
+	return &client
 }
 
 func initOAuth() {
-	config := readConfig()
 	startServer()
+	config := readConfig()
 	auth.SetAuthInfo(config.ClientID, config.ClientSecret)
 	url := auth.AuthURL(state)
 	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
 	token := <-ch
-	fmt.Println(token.Expiry.Unix())
-	client := auth.NewClient(token)
-	_, err := client.CurrentUser()
-	if err != nil {
-		log.Fatal(err)
-	}
-	config.AccessToken = token.AccessToken
-	config.RefreshToken = token.RefreshToken
-	config.TokenExpiry = token.Expiry.Unix()
-	writeConfig(config)
+	updateConfig(token)
 }
 
 func startServer() *http.Server {
-	srv := &http.Server{Addr: ":8080"}
+	srv := &http.Server{Addr: fmt.Sprintf(":%d", port)}
 	http.HandleFunc("/callback", completeAuth)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Got request for:", r.URL.String())
@@ -85,18 +86,7 @@ func completeAuth(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		log.Fatalf("State mismatch: %s != %s\n", st, state)
 	}
-	fmt.Fprintf(w, "Login Completed!")
+	fmt.Fprint(w, "Login Completed!")
 	// pass the token to channel
 	ch <- token
-}
-
-// from https://siongui.github.io/2015/04/13/go-generate-random-string/
-func getRandomString(strlen int) string {
-	rand.Seed(time.Now().UTC().UnixNano())
-	const chars = "abcdefghijklmnopqrstuvwxyz0123456789"
-	result := make([]byte, strlen)
-	for i := 0; i < strlen; i++ {
-		result[i] = chars[rand.Intn(len(chars))]
-	}
-	return string(result)
 }
